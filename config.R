c294aeee2b74dcc16ecfd220baccbdd0fa115ddd
JoFrhwld <JoFrhwld@gmail.com>
2023-02-09 17:59:29 -0500

    update to config and setup

--- config.R
+++ config.R
@@ -1,10 +1,10 @@
 renv::install("reticulate")
 # per https://github.com/rstudio/renv/issues/993
 
-reticulate::conda_create(
-  envname="./renv/python/condaenvs/renv-python",
-  packages=c("montreal-forced-aligner=2.0.6")
-  )
+# reticulate::conda_create(
+#    envname="./renv/python/condaenvs/renv-python",
+#    packages = "montreal-forced-aligner=2.0.6"
+#    )
 
 reticulate::use_condaenv(
   "./renv/python/condaenvs/renv-python", 
@@ -16,6 +16,7 @@ renv::use_python(
   type="conda"
 )
 
+# add git version manually
 reticulate::py_install(
   "git+https://github.com/JoFrhwld/FAVE@feature/parselmouth-fix",
   pip = T
---
+++ setup.R
@@ -0,0 +1,4 @@
+renv::restore()
+system("mfa configure -t renv/MFA")
+system("mfa model download acoustic english_us_arpa")
+system("mfa model download acoustic english_us_arpa")
